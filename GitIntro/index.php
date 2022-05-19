83bc2510a4a392746640c1d6fad02fd78bb204fc
Eric Charnesky <eric.charnesky@careevolution.com>
2022-05-19 00:36:20 -0400

    Week 1

---
+++ GitIntro/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h2>Hello World!</h2>";
+        echo "</br>Eric Charnesky, Doctor of Engineering in Automotive Systems and Mobility, "
+        . "I'm taking the class because I'm the prof";
+        echo "</br>I've been programming for 15 years";
+        echo "</br>I like playing Dunegons and Dragons";
+        echo "</br>We're hoping to adopt baby #7 in July";
+        echo "</br>I'm training for my 9th marathon, I run the Freep International in October";
+        ?>
+    </body>
+</html>
